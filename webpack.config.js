ac94031817d359b30af6df5cdbe29299631afb67
jaeyoung.choi <lactrious@gmail.com>
2021-05-15 11:22:22 +0900

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,22 @@ module.exports = {
   context: __dirname,
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: slsw.lib.entries,
-  devtool: slsw.lib.webpack.isLocal
-    ? "cheap-module-eval-source-map"
-    : "no-source-map",
   resolve: {
-    extensions: [".mjs", ".json", ".js", ".ts"],
-    symlinks: false,
-    cacheWithContext: false
+    extensions: [".mjs", ".json", ".ts", ".js"],
   },
   output: {
-    libraryTarget: "commonjs",
+    libraryTarget: "commonjs2",
     path: path.join(__dirname, ".webpack"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   target: "node",
-  externals: [/aws-sdk/],
   module: {
     rules: [
       {
-        test: /\.(tsx?)$/,
+        test: /\.ts$/,
         loader: "ts-loader",
-        exclude: [
-          [
-            path.resolve(__dirname, "node_modules"),
-            path.resolve(__dirname, ".serverless"),
-            path.resolve(__dirname, ".webpack")
-          ]
-        ],
-        options: {
-          transpileOnly: true,
-          experimentalWatchApi: true
-        }
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
